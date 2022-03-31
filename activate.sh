66bb8f47d820fb0494b928265ef42f2e82e650ac
lejinvarghese <lejinsnests@yahoo.com>
2022-03-30 21:07:55 -0400

    push assets

---
+++ activate.sh
@@ -0,0 +1,35 @@
+virtualenv --python=/usr/bin/python3.10 .venv
+source .venv/bin/activate
+
+# if you need cuda dependencies
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/11.6.1/local_installers/cuda-repo-ubuntu2004-11-6-local_11.6.1-510.47.03-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2004-11-6-local_11.6.1-510.47.03-1_amd64.deb
+sudo apt-key add /var/cuda-repo-ubuntu2004-11-6-local/7fa2af80.pub
+sudo apt-get update
+sudo apt-get -y install cuda
+
+
+sudo apt install imagemagick
+pip3 install -r requirements.txt
+
+git clone https://github.com/openai/CLIP
+git clone https://github.com/crowsonkb/guided-diffusion
+git clone https://github.com/assafshocher/ResizeRight.git
+git clone https://github.com/CompVis/latent-diffusion.git
+git clone https://github.com/CompVis/taming-transformers
+
+mv ./CLIP ./clip
+mv ./ResizeRight ./resize_right
+mv ./guided-diffusion ./guided_diffusion
+mv ./latent-diffusion ./latent_diffusion
+mv ./taming-transformers ./taming_transformers
+
+pip3 install -e ./clip
+pip3 install -e ./resize_right
+pip3 install -e ./guided_diffusion
+pip3 install -e ./latent_diffusion
+pip3 install -e ./taming_transformers
+
+# pip3 install --cache-dir=$TMP_DIR torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
