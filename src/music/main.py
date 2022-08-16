d9a421cfe268eb4e89705f4eb6ceb618529cf560
lejinvarghese <lejinsnests@yahoo.com>
2022-08-15 23:29:59 -0400

    uncommented but fixed package

--- src/music/main.py
+++ src/music/main.py
@@ -16,7 +16,7 @@ SPOTIFY_PLAYLIST_IDS = os.getenv("SPOTIFY_PLAYLIST_IDS")
 file_dir = Path(__file__).parent.absolute()
 
 
-# sp_client = SpotifyClient(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET)
+sp_client = SpotifyClient(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET)
 
 
 class MusicGraph:
@@ -107,24 +107,28 @@ class MusicGraph:
 
 
 def main():
-    # playlist_ids = SPOTIFY_PLAYLIST_IDS.split(",")
+    playlist_ids = SPOTIFY_PLAYLIST_IDS.split(",")
 
-    # attributes = {}
+    attributes = {}
 
-    # tracks = pd.DataFrame()
-    # for p_id in playlist_ids:
-    #     tracks = tracks.append(sp_client.get_playlist_tracks(p_id))
-    # attributes["tracks"] = tracks
-    # attributes["features"] = sp_client.get_track_info(attributes.get("tracks"))
-    # attributes["genres"] = sp_client.get_genres(attributes.get("tracks"))
+    tracks = pd.DataFrame()
 
-    # for name, data in attributes.items():
-    #     data.to_parquet(
-    #         f"{file_dir}/data/{name}.parquet",
-    #         engine="pyarrow",
-    #         compression="gzip",
-    #         index=False,
-    #     )
+    for p_id in playlist_ids:
+        tracks = tracks.append(sp_client.get_playlist_tracks(p_id))
+    attributes["tracks"] = tracks
+    attributes["features"] = sp_client.get_track_info(
+        attributes.get("tracks")
+    )
+    attributes["genres"] = sp_client.get_genres(attributes.get("tracks"))
+
+    for name, data in attributes.items():
+        data.to_parquet(
+            f"{file_dir}/data/{name}.parquet",
+            engine="pyarrow",
+            compression="gzip",
+            index=False,
+        )
+
 
     genres = pd.read_parquet(f"{file_dir}/data/genres.parquet")
 
